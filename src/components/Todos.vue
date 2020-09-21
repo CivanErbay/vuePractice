3161c9b4ed5ba84a5a5b9d7c70ce3298c2e088b2
navic1992 <civan.erbay@web.de>
2020-09-21 17:56:11 +0200

    Add Todos.vue component

---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>^
+        <h1>Todos</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos" //component Name which will be used for Export
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
